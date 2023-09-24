2ca781303840ce7324797af4cfcbdc277b073f9a
ALL-Ventura <140228584+ALL-Ventura@users.noreply.github.com>
2023-09-24 02:58:37 +0100

    OOP Optimization
    - The data requests are now encapsulated as properties.
    - The  are now different parsing function for each data type.
    - Now control function to simplify bulk requests

--- XTB_api.py
+++ XTB_api.py
@@ -1,140 +1,197 @@
-import websocket
 import json
-import os
 import pandas as pd
-
 import modules.tools as t
+import os
+import websocket
 
+from pandas import DataFrame
 
+# Read the API documentation for more information http://developers.xstore.pro/documentation/
 class XTB():
+
+    """
+    This class requests information from the XTB Websocket using your Xstantion Credentials.
+    Please fill you userID and password on the config file.
+
+    You can retrieve data by calling properties individually or by using the control method.
+    
+    Parsing functions have a default and an optional block of code for customizability.
+    
+    """
 
     def __init__(self) -> None: 
         # self.ssid = "" To be implemeented with the Stream Websocket
-        self.ws = "" # websocket Class
-        self.data = {} # Data retrieved by get()
-        self.command_holder = "" # Temporarily holds commands for Get()
-        self.import_commands()   
-        self.connect_real() 
+        self._wss = None # websocket Class
+        self._commands = None
+        self._fetch_resources()   
+        self._connect_wss() 
+ 
   
-    def import_commands(self):
+    def _fetch_resources(self):
         c_path = os.path.join(os.path.abspath("./"), "resources", "xtb_config.json")
         with open (c_path) as c_file:
-            self.commands = json.load(c_file)
+            self._commands = json.load(c_file)
 
 
-    def get_command(self, command:str):
-        self.command_holder = json.dumps(self.commands[command])
+    def _send_command(self, command:str):
+        """
+        Converts dictionary commands into json string and sends them to the websocket
+        
+        """
+        json_string_command = json.dumps(self._commands[command])
+        self._wss.send(json_string_command)
+        return self._wss.recv()
 
 
-    def connect_real(self):
-        # CONNECTS TO THE WEBSOCKET
-
-        # websocket.enableTrace(True) - For DebugginG
-        WEBSOCKET_URN = "wss://ws.xtb.com/real" # Stream websocket to be implemented: "wss://ws.xtb.com/realStream"
-        self.ws = websocket.create_connection(WEBSOCKET_URN)
+    def _connect_wss(self):
+    
+        # CONNECTS TO THE WEBSOCKET 
+        # websocket.enableTrace(True) # FOR DEBUGGING
+        WEBSOCKET_URN = "wss://ws.xtb.com/real"
+        self._wss = websocket.create_connection(WEBSOCKET_URN)
         
         # SENDS LOGIN COMMAND
-        self.get_command("login")
-        self.ws.send(self.command_holder)
-        in_tag = self.ws.recv()
-        in_tag = in_tag.split('"')
-        # self.ssid = in_tag[5] To be implemented with the Stream Websocket
-        print("-------------------------", in_tag[9])
-   
-   
-    def logout(self):
+        self._send_command("login")
+    
 
-        # SENDS LOGOUT COMMAND
-        self.get_command("logout")
-        self.ws.send(self.command_holder)
-        out_tag = self.ws.recv()
-        out_tag = out_tag.split('"')[5]
-        print("-------------------------", out_tag)
-
-      
-    def get(self, command:str):
+    def _get(self, command:str):
         """ 
-        Sends chosen command to request data and store it in the Class
+        Sends chosen command to the wss and returns the received data
         
         Command list: 
         > symbols: gets all the symbols available (>5k entries!)
         > trades: gets the open positions.
         > margin: gets the balance/margin details"""
 
-        if command.lower() in self.commands:
-            self.get_command(command.lower())
-            self.ws.send(self.command_holder)
-            work_data = self.ws.recv()
-            self.data[command] = json.loads(work_data)
-        else:
-            raise KeyError ("Invalid command")
+        data = self._send_command(command)
+        data = json.loads(data)
+
+        return data
         
-        
-    def parse(self):     
-        """
-        Parses the data stored in self.data into DatadFrames.
-        Use it after storing all the data you need
-        """   
-        if self.data == {}:
-            raise ValueError ("No data stored in the Class")
-        
-        # AVAILABLE COMMANDS
-        SYMBOLS_CMD = "symbols"
-        TRADES_CMD = "trades"
-        MARGIN_CMD = "margin"
-        
-        for command in self.data:
-            work_data = self.data[command]
 
-            # PARSE SYMBOLS --------------------------------------------------------------------
-            if command == SYMBOLS_CMD:
-                work_data = pd.DataFrame(work_data["returnData"]) 
+    @property
+    def trades(self) -> json:
+        return self._get("trades")
 
-            # PARSE TRADES --------------------------------------------------------------------
-            elif command == TRADES_CMD:
-                work_data = pd.json_normalize(work_data["returnData"])
-                work_data = work_data[["nominalValue", "symbol"]]
+    
+    @property
+    def symbols(self) -> json:
+        return self._get("symbols")
+    
+    @property
+    def margin(self) -> json:
+        return self._get("margin")
+      
 
-                # --- LOAD TICKER DATABASE
-                td_path = os.path.join(os.path.abspath("./"), "resources", "XTB_Labels.csv")
-                TD = pd.read_csv(td_path)
-                
-                # --- PROCESS DATA
-                work_data = pd.merge(work_data, TD, on="symbol", how= "inner")
-                work_data["categoryName"] = "ETF - Stocks" * len(work_data)
-                work_data = work_data [["categoryName", "nominalValue", "description"]]
+    def parse_trades(self, trades, custom=False) -> DataFrame:
+        """ Parses the json data into dataframes"""   
 
-            # PARSE MARGIN --------------------------------------------------------------------
-            elif command == MARGIN_CMD:
-                work_data = pd.DataFrame.from_dict(work_data["returnData"], orient="index", columns=["Value"]).T
-                work_data = work_data [["credit", "balance", "currency"]]
-                work_data["credit"] = "Cash"
+        df = pd.json_normalize(trades["returnData"])
 
-            # STORE THE PARSED DATA AS CLASS ATRIBUTES
-            setattr(self, command, work_data)
+        if custom is True:
+
+            df["equity"] = df["nominalValue"] + df["profit"]
+            df = df[["equity", "symbol"]]
             
 
+            # --- LOAD TICKER DATABASE
+            td_path = os.path.join(os.path.abspath("./"), "resources", "XTB_Labels.csv")
+            TD = pd.read_csv(td_path)
+            
+            # --- PROCESS DATA
+            df = pd.merge(df, TD, on="symbol", how= "inner")
+            df["categoryName"] = "ETF - Stocks" * len(df)
+            df = df [["categoryName", "equity", "description"]]
 
-# Sample code --- see output in /sample data
-
-api = XTB()
-
-api.get("trades")
-api.get("margin")
-api.get("symbols")
-api.parse()
-api.logout()
-
-with open("xtb_allData.json", "w") as save_file:
-    json.dump(api.data, save_file, indent=4)
-
-api.trades.to_excel("xtb_trades.xlsx")
-api.margin.to_excel("xtb_margin.xlsx")
-api.symbols.to_csv("xtb_symbols.csv")
-
-# Output stored in /sample data
+        return df
 
 
+    def parse_margin(self, margin, custom=False) -> DataFrame:
+        """ Parses the json data into dataframes"""   
+
+        df = pd.DataFrame.from_dict(margin["returnData"], orient="index", columns=["Value"]).T
+        
+        if custom is True:
+
+            df = df [["credit", "balance", "currency"]]
+            df["credit"] = "Cash"
+
+        return df
+
+
+    def parse_symbols(self, symbols, custom=False) -> DataFrame:
+        """ Parses the json data into dataframes"""     
+        df = DataFrame(symbols["returnData"])
+
+        if custom is True:
+            pass
+
+        return df
+
+
+    def control(self, trades:bool=False, margin:bool=False, symbols:bool=False, parse:bool=False, custom_parse:bool=False) -> dict [str, str|float|DataFrame]:
+        """"
+        This method controls the entire class in one go. \n
+        trades: requests the trades \n
+        margin: requests the margin \n
+        symbols: requests the symbols \n
+        parse: parses the requested data into dataframes \n
+        custom_parse: runs the optional block of code in the parse functions \n
+         \n
+        **settings = {"trades":True, "margin":True, "symbols":False, "parse": True, "custom_parse": True, } \n
+         \n
+       Returns a dictionary containing the requested json data or Dataframes (if parsed) \n
+        """
+        output = {}
+
+        if trades is True:
+            data = self.trades
+            if parse is True:
+                data = self.parse_trades(data, custom=custom_parse)
+            
+            output["trades"] = data
+
+        if margin is True:
+            data = self.margin
+            if parse is True:
+                data = self.parse_margin(data, custom=custom_parse)
+            output["margin"] = data
+
+        if symbols is True:
+            data = self.symbols
+            if parse is True:
+                data = self.parse_symbols(data, custom=custom_parse)
+            
+            output["symbols"] = data
+
+        return output
 
 
 
+# SAMPLE CODE
+xtb = XTB()
+
+# 1- Individual Properties
+margin = xtb.margin
+margin = xtb.parse_margin(margin, custom=True)
+
+trades = xtb.trades
+trades = xtb.parse_trades(trades, custom=True)
+
+# 2 - Control Function
+  
+xtb_settings = {
+                "trades":True, 
+                "margin":True, 
+                "symbols":False, 
+                "parse": True, 
+                "custom_parse": True, 
+                }
+
+xtb_dict = xtb.control(**xtb_settings)
+
+# This will return a dictionary with 2 dataframes:
+#{"trades": DataFrame,
+#"margin": DataFrame,}
+
+              
